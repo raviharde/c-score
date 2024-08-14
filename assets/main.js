fc399226d663d6f2c6fd0fe3e53a494ee076c164
ravi <ravindra@stavyah.com>
2024-08-14 20:07:26 +0530

    Mobile Menu Steps icons and content changes

--- assets/main.js
+++ assets/main.js
@@ -1,8 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
     const menuToggle = document.getElementById('menu-toggle');
+    const mobileMenuToggle = document.getElementById('mobile-menu-toggle');
     const navMobile = document.querySelector('.nav-mobile');
 
     menuToggle.addEventListener('click', () => {
+        navMobile.classList.toggle('active');
+    });
+    mobileMenuToggle.addEventListener('click', () => {
         navMobile.classList.toggle('active');
     });
 });
